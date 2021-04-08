25e84ca5e99068f20938a82c721f7ade825d99a1
ahnw00 <ahnw00@naver.com>
2021-04-08 23:07:07 +0900

    asdf

--- KoalaArcherProject/Assets/Scripts/ManagerScripts/StageManager.cs
+++ KoalaArcherProject/Assets/Scripts/ManagerScripts/StageManager.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
+    GameManager gameManager;
+    SaveDataClass saveData;
+    List<StageClass> stageList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameManager.singleTon;
+        saveData = gameManager.saveData;
+        stageList = saveData.stageList;
+
+        if(stageList[0] == null)
+        {
+            stageList[0] = new StageClass();
+            stageList[0].aimSpeed = 0;
+            stageList[0].bestScore = 0;
+
+            stageList.Add(stageList[0]);
+        }
     }
 
     // Update is called once per frame
