100e58e931c8e1a54deda51d0b1a2d7a99dceba0
eugene028 <67894159+eugene028@users.noreply.github.com>
2021-04-22 16:04:46 +0900

    ReulstPopUp2

--- KoalaArcherProject/Assets/Scripts/GameSceneScripts/ResultPopUp/ScoreAndImage.cs
+++ KoalaArcherProject/Assets/Scripts/GameSceneScripts/ResultPopUp/ScoreAndImage.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ScoreAndImage : MonoBehaviour
 {
-    // Start is called before the first frame update
+    InGameManager inGameManager;
+    ScoreScript scoreScript;
+    CounterKoala counterScoreScript;
+    public Text resultScore;
+    public GameObject resultImage;
+    public Image completedImage;
+    public Image failedImage;
     void Start()
     {
-        
+        inGameManager=FindObjectOfType<InGameManager>();
+        scoreScript=FindObjectOfType<ScoreScript>();
+        counterScoreScript=FindObjectOfType<CounterKoala>();
+        StartCoroutine(ResultCoroutine());
+        StartCoroutine(ResultSpriteCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ResultCoroutine()
     {
-        
+        while(true)
+        {
+            yield return null;
+            if(inGameManager.orderOfShot - 1 == 9)
+            {
+                resultScore.Text = scoreScript.resultScore.ToString();
+                break;
+            }
+        }
+    }
+    
+    IEnumerator ResultSpriteCoroutine(){
+        while(true)
+        {
+            yield return null;
+            if(inGameManager.orderOfShot - 1 == 9 && scoreScript.resultScore > counterScoreScript.resultScoreOfCounter)
+            {
+                resultImage.Image = completedImage;
+                break;
+            }
+            
+            else if(inGameManager.orederOfShot - 1 == 9 && scoreScript.resultScore <= counterScoreScript.resultScoreOfCounter)
+            {
+                resultImage.Image=failedImage;
+                break;
+            }
+        }
     }
 }
