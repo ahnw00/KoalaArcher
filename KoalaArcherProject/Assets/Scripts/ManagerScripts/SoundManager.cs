85282f9933a739b3be6b61fd1102f61ae9867cfc
ahnw00 <ahnw00@naver.com>
2021-05-01 03:30:55 +0900

    SoundManager

---
+++ KoalaArcherProject/Assets/Scripts/ManagerScripts/SoundManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager inst;
+
+    public AudioSource bgmSource;
+    public AudioSource effectSource;
+    public AudioSource buttonSource;
+
+    public AudioClip mainBGM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(inst == null)
+        {
+            inst = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        bgmSource.clip = mainBGM;
+        bgmSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
