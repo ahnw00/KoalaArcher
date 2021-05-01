15e0c640ee69abfe475665023bd61c93d059aab4
dynene <ekdlsb02@gmail.com>
2021-05-01 15:47:50 +0900

    .gitignore location changed & mainscene setting-added slider

---
+++ KoalaArcherProject/Assets/Scripts/SoundScripts/SoundController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundController : MonoBehaviour
+{
+    public Slider bgmSlider;
+    public Slider effectSlider;
+
+    void Update()
+    {
+        //value값을 계속 맞춰주는 작업
+        //여기서 bgm, soundeffect를 모두 관리
+
+        //bgmSource.volume = bgmSlider.value;
+        //effectSource.volume = effectSlider.value;
+    }
+}
