b597851a552d9b465b1e99f77efec4982e52c311
eugene02876 <eugene02876@gmail.com>
2021-05-22 00:04:34 +0900

    MainScene
    MainScene StartBtn / ContinueBtn

--- KoalaArcherProject/Assets/Scripts/ClassScripts/SaveDataClass.cs
+++ KoalaArcherProject/Assets/Scripts/ClassScripts/SaveDataClass.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-//여기다가 새로하기 이어하기에 대한 정보(첫 플레이인지 아닌지)
-//
 
 
 [System.Serializable]
--- KoalaArcherProject/Assets/Scripts/SceneLoadScript/NewSaveBtn.cs
+++ KoalaArcherProject/Assets/Scripts/SceneLoadScript/NewSaveBtn.cs
@@ -2,11 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class NewSaveBtn : MonoBehaviour
 {
-    public void LoadStageScene()
+    GameManager gameManager;
+    SaveDataClass saveData;
+    public void LoadCutScene()
     {
+        gameManager = GameManager.singleTon;
+        saveData = gameManager.saveData;
         SceneLoadManager.instance.LoadScene("CutScene");
     }
+    public void LoadStageScene()
+    {
+        gameManager = GameManager.singleTon;
+        saveData = gameManager.saveData;
+        SceneLoadManager.instance.LoadScene("StageScene");
+    }
+    public void SetFirstOfPlay()
+    {
+        gameManager = GameManager.singleTon;
+        saveData = gameManager.saveData;
+        saveData.isFirstTimeOfPlay = false;
+    }
+    
 }
