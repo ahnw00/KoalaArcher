85b4c13a59f3c855a5d72ec807cc01d44f6b3933
eugene02876 <eugene02876@gmail.com>
2021-05-22 14:30:09 +0900

    MainScene&LoadData

--- KoalaArcherProject/Assets/Scripts/SceneLoadScript/NewSaveBtn.cs
+++ KoalaArcherProject/Assets/Scripts/SceneLoadScript/NewSaveBtn.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class NewSaveBtn : MonoBehaviour
 {
+    GameManager gameManager;
+    SaveDataClass saveData;
+    public void LoadCutScene()
+    {
+        gameManager = GameManager.singleTon;
+        gameManager.saveData=new SaveDataClass();
+        saveData=gameManager.saveData;
+        saveData.isFirstTimeOfPlay=false;
+        saveData=gameManager.saveData;
+        gameManager.Save();
+        SceneLoadManager.instance.LoadScene("CutScene");
+    }
     public void LoadStageScene()
     {
-        SceneLoadManager.instance.LoadScene("CutScene");
+        gameManager= GameManager.singleTon;
+        saveData=gameManager.saveData;
+        SceneLoadManager.instance.LoadScene("StageScene");
     }
 }
