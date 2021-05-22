70693c14d9e0d11671fb0c547b00183e617d9e3e
eugene02876 <eugene02876@gmail.com>
2021-05-22 14:12:00 +0900

    Revert "MainScene"
    This reverts commit b597851a552d9b465b1e99f77efec4982e52c311.

--- KoalaArcherProject/Assets/Scripts/SceneLoadScript/NewSaveBtn.cs
+++ KoalaArcherProject/Assets/Scripts/SceneLoadScript/NewSaveBtn.cs
@@ -2,29 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System;
 
 public class NewSaveBtn : MonoBehaviour
 {
-    GameManager gameManager;
-    SaveDataClass saveData;
-    public void LoadCutScene()
-    {
-        gameManager = GameManager.singleTon;
-        saveData = gameManager.saveData;
-        SceneLoadManager.instance.LoadScene("CutScene");
-    }
     public void LoadStageScene()
     {
-        gameManager = GameManager.singleTon;
-        saveData = gameManager.saveData;
-        SceneLoadManager.instance.LoadScene("StageScene");
+        SceneLoadManager.instance.LoadScene("CutScene");
     }
-    public void SetFirstOfPlay()
-    {
-        gameManager = GameManager.singleTon;
-        saveData = gameManager.saveData;
-        saveData.isFirstTimeOfPlay = false;
-    }
-    
 }
